0e1da37e109e249feea8f88f4b3a569661105d90
yravan <71646380+yravan@users.noreply.github.com>
2022-06-28 19:30:14 +0300

    Update getdependencies.py

--- getdependencies.py
+++ getdependencies.py
@@ -1,9 +1,8 @@
-!apt-get install poppler-utils 
-!pip install pdf2image # to convert pdf's to images
-!apt-get install inkscape # to convert pdf's to svg'sf Changes: changed from pip to apt-get
-!pip install IPython # interactive python shell
-!apt-get install tesseract-ocr # Changes: changed from pip to apt-get
-!pip install pytesseract # optical character recognition tool
-!pip install pillow # image processing library
-!pip install svgpathtools #for analyzing svg
-!pip install cairosvg # for converting to png
+pip install --upgrade pip
+pip install poppler-utils 
+pip install pdf2image # to convert pdf's to images
+pip install IPython # interactive python shell
+pip install pytesseract # optical character recognition tool
+pip install pillow # image processing library
+pip install svgpathtools #for analyzing svg
+pip install cairosvg # for converting to png
