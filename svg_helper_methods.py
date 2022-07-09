89157b452635eadbedffe23206f11baac804173e
Kevin Wen <kwen3@me.com>
2022-07-09 19:57:23 -0400

    Updated remove_from_list method

--- svg_helper_methods.py
+++ svg_helper_methods.py
@@ -18,28 +18,32 @@ def string_to_dict(s):
 def dict_to_string(d):
 	return ";".join(f"{key}:{val}" for key, val in d.items()) + ";"
 
-def remove_from_list(l, indices):
-  """
-  returns a list with some indexes removed
-  e.g remove_from_list([50, 60, 70, 80], [1, 3]) should return [50, 70]
-  This is particularly useful to remove some SVG paths that are not valid (or not important)
-  Common example:
-    paths, attributes, svg_attributes = svg2paths2(svg_path)
-    to_delete = [] #indices
-    for i, path in enumerate(paths):
-      if not a good/interesting path:
-        to_delete.append(i)
-    new_paths = remove_from_list(new_paths, to_delete)
-    new_attributes = remove_from_list(new_attributes, to_delete)
-    #Now can use new_paths, new_attributes and svg_attributes to create a new SVG/PDF
-  -Raul  
-  """
 
-  indices = [-1] + indices + [len(l)]
-  res = []
-  for i in range(len(indices) -1):
-    res +=  l[indices[i]+1: indices[i+1]]
-  return res
+def remove_from_list(list, indices_to_delete):
+	"""
+	returns a list with some indexes removed
+	e.g remove_from_list([50, 60, 70, 80], [1, 3]) should return [50, 70]
+	This is particularly useful to remove some SVG paths that are not valid (or not important)
+	Common example:
+	paths, attributes, svg_attributes = svg2paths2(svg_path)
+	to_delete = [] #indices
+	for i, path in enumerate(paths):
+		if not a good/interesting path:
+		to_delete.append(i)
+	new_paths = remove_from_list(new_paths, to_delete)
+	new_attributes = remove_from_list(new_attributes, to_delete)
+	#Now can use new_paths, new_attributes and svg_attributes to create a new SVG/PDF
+	-Raul  
+	"""
+	result = []
+
+	for i, item in enumerate(list):
+		if i not in indices_to_delete:
+			result.append(item)
+	
+	return result
+
+
 
 def show_svg(filename, is_local=True):
   # display(SVG(url='http://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg'))
@@ -58,7 +62,7 @@ def visualize_all_paths(paths, attributes, svg_attributes, output, show = False)
 COLORS = ['red', 'green', 'blue', 'yellow', 'cyan', 'purple'] # colors in a given width, in order
 
 def get_real_path(path, attribute):
-  return  path_transform(path, parse_transform(attribute.get('transform', '')))
+  return path_transform(path, parse_transform(attribute.get('transform', '')))
 
 def is_door(path, attribute):
   """
@@ -78,7 +82,7 @@ def is_door(path, attribute):
     return False
   real_path = get_real_path(path, attribute)
   start, end = real_path.start, real_path.end
-  diff = start-end
+  diff = start - end
   segment_length = math.sqrt((diff.real)**2 + (diff.imag)**2)
   curve_length = real_path.length()
   if segment_length == 0:
@@ -89,23 +93,23 @@ def is_door(path, attribute):
  
 
 def find_and_update_doors(paths, attributes):
-  new_paths= paths.copy()
-  new_attributes = attributes.copy()
+	new_paths= paths.copy()
+	new_attributes = attributes.copy()
 
-  door_ids = []
-  to_delete_segments = []
-  for i, (path, attribute) in enumerate(zip(new_paths, new_attributes)):
-    if not is_door(path, attributes[i]):
-      continue
-    print("Found door at position i = ", i)
-    # Finding what path to add and
-    to_delete_segments.append(i)
-    door_ids.append(i)
- 
-  print("Number of doors found:", len(door_ids))
-  new_paths = remove_from_list(new_paths, to_delete_segments)
-  new_attributes = remove_from_list(new_attributes, to_delete_segments)
-  return new_paths, new_attributes
+	door_ids = []
+	to_delete_segments = set()
+
+	# iterate over paths and attributes
+	for i, (path, attribute) in enumerate(zip(new_paths, new_attributes)):
+		if is_door(path, attribute):
+			print("Found door at position i = ", i)
+			to_delete_segments.add(i)
+			door_ids.append(i)
+
+	print("Number of doors found:", len(door_ids))
+	new_paths = remove_from_list(new_paths, to_delete_segments)
+	new_attributes = remove_from_list(new_attributes, to_delete_segments)
+	return new_paths, new_attributes
 
 
 if __name__ == '__main__':
