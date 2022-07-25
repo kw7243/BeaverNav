1c687dd1645f8d5327140078c6c706cdbdb00198
Kevin Wen <kwen3@me.com>
2022-07-25 19:19:24 -0400

    Clean door detection scripts and organize directory

--- door_detection/svg_helper_methods.py
+++ door_detection/svg_helper_methods.py
@@ -6,126 +6,128 @@ import numpy as np
 from IPython.display import SVG, display
 from svgpathtools import svg2paths2, wsvg, Path
 
-"""
-Two functions that convert string of the form 'x:1;y:2;z:3;' to a dictionary of the form {x:1, y:2, z:3}, and viceversa
-Useful to update style of an SVG. Used on the 'update_attribute' function below
--Raul
-"""
 
 def remove_from_list(l, indices_to_delete):
-  """
-  returns a list with some indexes removed
-  e.g remove_from_list([50, 60, 70, 80], [1, 3]) should return [50, 70]
-  This is particularly useful to remove some SVG paths that are not valid (or not important)
-  Common example:
-    paths, attributes, svg_attributes = svg2paths2(svg_path)
-    to_delete = [] #indices
-    for i, path in enumerate(paths):
-      if not a good/interesting path:
-        to_delete.append(i)
-    new_paths = remove_from_list(new_paths, to_delete)
-    new_attributes = remove_from_list(new_attributes, to_delete)
-    #Now can use new_paths, new_attributes and svg_attributes to create a new SVG/PDF
-  -Raul  
-  """
-  res = []
+    """
+    Given a list l and a set of indicies to delete,
+    return a new list w/o elements at those indices
+    """
+    res = []
 
-  for i,item in enumerate(l):
-    if i in indices_to_delete:
-      continue
-    res.append(item)
+    for i, item in enumerate(l):
+        if i not in indices_to_delete:
+            res.append(item)
 
-  return res
-
+    return res
 
 def show_svg(filename, is_local=True):
-  # display(SVG(url='http://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg'))
-  if is_local:
-    display(SVG(filename=filename))
-  else:
-    display(SVG(url=filename))
-"""
-Saves given svg to a temporary file and shows it
-"""
+    if is_local:
+        display(SVG(filename=filename))
+    else:
+        display(SVG(url=filename))
 
-def visualize_all_paths(paths, attributes, svg_attributes, output, show = False):
-  wsvg(paths, filename=output, attributes=attributes, svg_attributes=svg_attributes)
-  if show:
-    show_svg(output)
 
-COLORS = ['red', 'green', 'blue', 'yellow', 'cyan', 'purple'] # colors in a given width, in order
+def visualize_paths(paths, attributes, svg_attributes, file_name, show=False):
+    """
+    Saves given svg to a temporary file and shows it
+    """
+    wsvg(paths, filename=file_name, attributes=attributes, svg_attributes=svg_attributes)
+    if show:
+        show_svg(file_name)
+
 
 def get_real_path(path, attribute):
-  return  path_transform(path, parse_transform(attribute.get('transform', '')))
+    return path_transform(path, parse_transform(attribute.get('transform', '')))
+
 
 def is_door(path, attribute):
-  """
-  Returns wheter a given path is a door by checking
-  1. it's not closed
-  2. it's continuous
-  3. it's curved
-  4. The curve length of the path / linear length = pi / 2sqrt(2) [what you'd expect in a quarter of a circle]
-  """
-  if len(path) == 0:
-    return False
-  if not path.iscontinuous():
-    return False
-  if path.isclosed() or "C" not in attribute['d']:
-    return False
-  if "C" not in attribute['d']:
-    return False
-  real_path = get_real_path(path, attribute)
-  start, end = real_path.start, real_path.end
-  diff = start-end
-  segment_length = math.sqrt((diff.real)**2 + (diff.imag)**2)
-  curve_length = real_path.length()
-  if segment_length == 0:
-    return False
-  q = curve_length / segment_length #should be pi / 2sqrt(2) coz math
-  good_q = math.pi / (2 * math.sqrt(2))
-  return abs(q/good_q - 1) < 1e-2
+    """
+    Returns True if given path is a door else False.
+
+    Conditions for a door:
+    - Not empty
+    - Continuous
+    - Not closed
+    - Curved (designated by presence of a "C" command AKA curveto)
+    - Arc length of the path/linear length = pi/2sqrt(2) 
+      [what you'd expect in a quarter of a circle]
+    """
+    if not(path.iscontinuous() and not path.isclosed() and 
+            ("C" in attribute["d"] or "c" in attribute["d"])):
+        return False
+    
+    real_path = get_real_path(path, attribute)
+
+    diff = real_path.start - real_path.end
+    segment_length = math.sqrt((diff.real)**2 + (diff.imag)**2) # get distance between endpoints of path
+    curve_length = real_path.length() # get arc length of path
+
+    if segment_length == 0:
+        return False
+    
+    test_ratio = curve_length / segment_length
+    ideal_ratio1 = math.pi / (2*math.sqrt(2)) # for a quarter circle arc
+    ideal_ratio2 = math.pi/2 + 1 # for quarter circle arc + radial line
+
+    # Is door only if ratios within 1% of ideal ratios
+    return abs(test_ratio/ideal_ratio1 - 1) < 1e-1 or abs(test_ratio/ideal_ratio2 - 1) < 1e-1
  
 
-def find_and_update_doors(paths, attributes):
-	"""
-	Given a list of Path objects 
-	and corresponding list of attribute dictionaries,
-	find doors and remove them
+def remove_doors(paths, attributes):
+    """
+    Given a list of Path objects 
+    and corresponding list of attribute dictionaries,
+    find door paths and remove them from both the image's
+    paths and attributes 
 
-	Returns new list of paths and attributes w/
-	doors removed
-	"""
-	door_ids = []
-	to_delete_segments = set()
+    Returns new list of paths and attributes w/
+    doors removed
+    """
+    indices_to_delete = set()
 
-	# iterate over paths and attributes
-	for i, (path, attribute) in enumerate(zip(paths, attributes)):
-		if is_door(path, attribute):
-			print("Found door at position i = ", i)
-			to_delete_segments.add(i)
-			door_ids.append(i)
+    # iterate over paths and attributes
+    for i, (path, attribute) in enumerate(zip(paths, attributes)):
+        if is_door(path, attribute):
+            # print("Found door at position i = ", i)
+            indices_to_delete.add(i)
 
-	print(f"Number of doors found: {len(door_ids)}")
-	new_paths = remove_from_list(paths, to_delete_segments)
-	new_attributes = remove_from_list(attributes, to_delete_segments)
-	return new_paths, new_attributes
-
-
-def remove_empty_paths_attributes(paths,attributes):
-  indices_to_remove = set()
-
-  for i, path in enumerate(paths):
-    if path == Path():
-      indices_to_remove.add(i)
-
-  paths = remove_from_list(paths, indices_to_remove)
-  attributes = remove_from_list(attributes, indices_to_remove)
-
-  return paths,attributes
+    # print(f"Number of doors found: {i}")
+    new_paths = remove_from_list(paths, indices_to_delete)
+    new_attributes = remove_from_list(attributes, indices_to_delete)
+    return new_paths, new_attributes
 
 
-if __name__ == '__main__':
-  paths, attributes, svg_attributes = svg2paths2('1_2.svg')
-  paths, attributes = remove_empty_paths_attributes(paths,attributes)
-  new_paths, new_attributes = find_and_remove_doors(paths,attributes)
-  visualize_all_paths(new_paths,new_attributes,svg_attributes,'output_final.svg',show=True)
+def remove_empty_paths(paths, attributes):
+    """
+    Conversion to SVG sometimes results in empty paths,
+    which interfere w/ many operations
+
+    Returns new paths and attributes w/ empty paths removed/
+    """
+    indices_to_remove = set()
+
+    for i, path in enumerate(paths):
+        if path == Path():
+            indices_to_remove.add(i)
+
+    paths = remove_from_list(paths, indices_to_remove)
+    attributes = remove_from_list(attributes, indices_to_remove)
+
+    return paths, attributes
+
+
+def get_doors(paths, attributes):
+    """
+    Returns new paths and attributes of those 
+    corresponding to doors
+    """
+    to_delete = set()
+
+    for i, (path, attribute) in enumerate(zip(paths, attributes)):
+        if not is_door(path, attribute):
+            to_delete.add(i)
+    
+    door_paths = remove_from_list(paths, to_delete)
+    door_attributes = remove_from_list(attributes, to_delete)
+
+    return door_paths, door_attributes
