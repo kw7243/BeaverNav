0378c29bf9f6a49ef2f0fc92d7efddc606cd607b
VasuKaker <vasuk@mit.edu>
2022-12-08 12:12:18 -0500

    Minor fixes to prune_graphs.py and partial_demo.py

--- floorplan_to_graph/prune_graphs.py
+++ floorplan_to_graph/prune_graphs.py
@@ -17,8 +17,9 @@ temp_dir = "full_pipeline_files_test/temp_files"
 with open(f"{reduced_res_png_dir}/scaling_factors.json") as f:
     scaling_factors = json.load(f)
 abstract_graph = "full_pipeline_files_test/special_feature_coordinates.json"
-with open( abstract_graph, 'r') as out:
+with open(abstract_graph, 'r') as out:
     special_features = json.load(out)
+
 
 def main():
     for graph_name in os.listdir(graph_storage_dir):
@@ -26,9 +27,16 @@ def main():
         print(floor_plan)
         # if floor_plan != "1_1":
         #     continue
+        # if floor_plan.split('_')[0] not in ['1', '3', '10']:
+        #     # print(floor_plan.split('-')[0])
+        #     continue
+        # if floor_plan.split('_')[1] not in ['1', '2']:
+        #     # print(floor_plan.split('-')[1])
+        #     continue
         if f"{floor_plan}_graph.pickle" in os.listdir(temp_dir):
             continue
-        pixel_graph = pickle.load(open(graph_storage_dir + '/' + graph_name, 'rb'))
+        pixel_graph = pickle.load(
+            open(graph_storage_dir + '/' + graph_name, 'rb'))
         print("loaded")
         scaling_factor = scaling_factors[floor_plan + ".png"]
         with open(txt_dir + '/' + floor_plan + '.json', 'r') as f:
@@ -37,8 +45,8 @@ def main():
         for node_type in special_feature_coords:
             if node_type not in ['sa', 'ea']:
                 for coord_set in special_feature_coords[node_type]:
-                    string_version = str((coord_set[0],coord_set[1]))
-                    room_locations.append([node_type,string_version])
+                    string_version = str((coord_set[0], coord_set[1]))
+                    room_locations.append([node_type, string_version])
         # print(room_locations)
         relevant_pixels = set()
         relevant_nodes = set()
@@ -47,7 +55,13 @@ def main():
             print("Iteration ")
             for iteration in range(2):
                 node_queue = room_locations.copy()
-                unreachable_nodes = defaultdict(0)
+
+                def return_zero():
+                    return 0
+
+                unreachable_nodes = defaultdict(return_zero)
+                # defaultdict on my laptop accepts a CALLABLE, does not accept an int
+
                 random.shuffle(node_queue)
                 while (len(node_queue) > 1):
                     start_location = node_queue[0][1]
@@ -56,36 +70,38 @@ def main():
                     end_location = node_queue[1][1]
                     end_location = (int(end_location.split(',')[0][1:]), int(
                         end_location.split(',')[1][:-1]))
-                    start_reduced = (int(start_location[0]//scaling_factor), int(start_location[1]//scaling_factor))
-                    end_reduced = (int(end_location[0]//scaling_factor), int(end_location[1]//scaling_factor))
+                    start_reduced = (
+                        int(start_location[0]//scaling_factor), int(start_location[1]//scaling_factor))
+                    end_reduced = (
+                        int(end_location[0]//scaling_factor), int(end_location[1]//scaling_factor))
                     try:
-                        path = Dijkstar_duplicated_graph(pixel_graph, start_reduced, end_reduced)
+                        path = Dijkstar_duplicated_graph(
+                            pixel_graph, start_reduced, end_reduced)
                     except:
                         unreachable_nodes[node_queue[0]] += 1
                         unreachable_nodes[node_queue[1]] += 1
                         random.shuffle(node_queue)
                         if max(unreachable_nodes.values()) < 3:
                             continue
-                    for (x,y), tag in path:
-                        relevant_pixels.add((x,y))
+                    for (x, y), tag in path:
+                        relevant_pixels.add((x, y))
 
                     node_queue.pop(0)
-                    node_queue.pop(0)  
-                    guarantee_check[start_location] = 1 
-                    guarantee_check[end_location] = 1 
+                    node_queue.pop(0)
+                    guarantee_check[start_location] = 1
+                    guarantee_check[end_location] = 1
                     # print(start_location,end_location)
         # print(relevant_pixels)
-        save_image_with_path_drawn(f"{reduced_res_png_dir}/{floor_plan}.png", f"{temp_dir}/{floor_plan}.png", relevant_pixels)
+        save_image_with_path_drawn(
+            f"{reduced_res_png_dir}/{floor_plan}.png", f"{temp_dir}/{floor_plan}.png", relevant_pixels)
 
         large_node_list = pixel_graph.get_data().copy()
         for node in large_node_list:
             if node[0] not in relevant_pixels:
                 pixel_graph.remove_node(node)
-        
+
         with open(f"{temp_dir}/{floor_plan}_graph.pickle", "wb") as f:
             pickle.dump(pixel_graph, f)
-        
-
 
 
 if __name__ == '__main__':
