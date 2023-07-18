b63eb9566af3f9b80dcfb77515ece8b03350d7b2
Kevin Wen <kwen1@mit.edu>
2023-07-17 18:10:40 -0700

    Reverted DirectionsScreen to local testing mode

--- app_dev/src/screens/DirectionsScreen.js
+++ app_dev/src/screens/DirectionsScreen.js
@@ -1,22 +1,43 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
-import React, { useState, useEffect } from 'react';
-import { get, post } from '../utilities';
+import React, { useState } from 'react';
+// import { get } from '../utilities';
 import DirectionsCard from '../components/DirectionsCard';
 import Map from '../components/Map';
 import tw from 'tailwind-react-native-classnames';
 
+const floorPlanQueries = [
+    {
+        floorPlan: "4_1",
+        start: "Building 4 entrance",
+        end: "Building 2",
+        src: require("../local_test/4_1.png")
+    },
+    {
+        floorPlan: "2_1",
+        start: "Building 2 entrance from building 4",
+        end: "Building 6",
+        src: require("../local_test/2_1.png")
+    },
+    {
+        floorPlan: "6_1",
+        start: "Building 6 entrance from building 2",
+        end: "Room 6-132",
+        src: require("../local_test/6_1.png")
+    }
 
-import { useSelector } from 'react-redux';
-import { selectOrigin, selectDestination } from '../slices/navSlice';
+];
+
+const get = (endpoint, floorPlanQuery) => {
+    return floorPlanQuery.src
+};
 
 const DirectionsScreen = () => {
-    const START_REQUEST_URL = "http://45.33.64.67/route";
-    const REQUEST_IMAGE_URL = "http://45.33.64.67/route/image";
+    const MULTIPLE_FLOORS_API = "INSERT HERE";
+    const SINGLE_FLOOR_API = "INSERT HERE";
 
     // `index` tracks the current floor plan and displayed directions
     const [index, setIndex] = useState(0);
-    const origin = useSelector(selectOrigin);
-    const destination = useSelector(selectDestination);
+
 
     /**
      * floorPlanQueries stores a floor plan and 2 locations
@@ -29,50 +50,20 @@ const DirectionsScreen = () => {
      *     floorPlan: "26_1",
      *     start: "Room 100"
      *     end: "Room 121"
-     *   },
-     *   {
-     *     floorPlan: "2_2",
-     *     start: "Room 43"
-     *     end: "Room 12"
      *   }
      * ]
      */
-    const [floorPlanQueries, setFloorPlanQueries] = useState([]);
-    const [floorPlanImg, setFloorPlanImg] = useState(undefined) // base64 rep of blob
+    // const [floorPlanQueries, setFloorPlanQueries] = useState([]);
 
     // Called when first loading DirectionsScreen
     // with origin + destination
-    useEffect(() => {
-        post(START_REQUEST_URL, { origin, destination }).then((floorQueries) => {
-            setFloorPlanQueries(floorQueries); // will have to check if GET response is an array
-        });
-        
-    }, []);
+    // useEffect(() => {
+    //     get(MULTIPLE_FLOORS_API).then((floorQueries) => {
+    //         setFloorPlanQueries(floorQueries); // will have to check if GET response is an array
+    //     });
+    // }, []);
     
-    useEffect(() => {
-        console.log(floorPlanQueries.length)
-        if (index < floorPlanQueries.length) {
-            console.log('getting image')
-            console.log(floorPlanQueries[index].image_data)
-            
-            // fetch(`${REQUEST_IMAGE_URL}?path=${encodeURIComponent(floorPlanQueries[index].image_data)}`).then(res => {
-            //     res.blob().then(blob => {
-            //         // Look at https://stackoverflow.com/questions/38506971/react-native-populate-image-with-blob-that-has-been-converted-to-a-url
-            //         const reader = new FileReader();
-            //         reader.readAsDataURL(blob);
-            //         reader.onloadend = function() {
-            //             const base64Data = reader.result;
-            //             console.log(`data:image/png;base64,${base64Data}`);
-            //             setFloorPlanImg(`data:image/png;base64,${base64Data}`);
-            //         };
-            //     })
-                
-            //     // res.text().then(base64 => setFloorPlanImg(base64))
-            // })
-        }
-    }, [floorPlanQueries])
-
-    // Assumming response type is URI
+    const floorPlanImage = get(SINGLE_FLOOR_API, floorPlanQueries[index]);
 
     return (
         <View>
@@ -85,9 +76,7 @@ const DirectionsScreen = () => {
                         height: "100%",
                     }} 
                     resizeMode="contain"
-                    source={{
-                        uri: `${REQUEST_IMAGE_URL}?path=${encodeURIComponent(floorPlanQueries[index].image_data)}`
-                    }}
+                    source={floorPlanImage}
                 />
                 }
                 {/* <Map/> */}
